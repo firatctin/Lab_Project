6c1bbfac63afc5035037f69fe83aa50c717ea9e0
cetin <firatcetin.h0nts@gmail.com>
2022-12-03 00:18:43 +0300

    Decision statements are strengthened

--- Lab_project.c
+++ Lab_project.c
@@ -6,7 +6,7 @@ int main() {
     //Lets declare the variables:
     float triangle1Ax,triangle1Ay,triangle1Bx,triangle1By,triangle1Cx,triangle1Cy,triangle2Ax,triangle2Ay,triangle2Bx,triangle2By,triangle2Cx,triangle2Cy,triangle3Ax,triangle3Ay,triangle3Bx,triangle3By,triangle3Cx,triangle3Cy;
     float triangle1Side1,triangle1Side2,triangle1Side3,triangle2Side1,triangle2Side2,triangle2Side3,triangle3Side1,triangle3Side2,triangle3Side3,Px,Py,Area,Area1,Area2,Area3;
-    
+    float AreaTriangle1,AreaTriangle2,AreaTriangle3,PAB1,PBC1,PAC1,PAB2,PBC2,PAC2,PAB3,PBC3,PAC3,sum1,sum2,sum3;
     while(1){
         //For triangle 1; I will take the points
         printf("\nEnter Ax1:\n");
@@ -25,7 +25,8 @@ int main() {
         triangle1Side1 = sqrt(pow(triangle1Ax-triangle1Bx,2) + pow(triangle1Ay-triangle1By,2));
         triangle1Side2 = sqrt(pow(triangle1Ax-triangle1Cx,2) + pow(triangle1Ay-triangle1Cy,2));
         triangle1Side3 = sqrt(pow(triangle1Cx-triangle1Bx,2) + pow(triangle1Cy-triangle1By,2));
-        if ((abs(triangle1Side1-triangle1Side2) < triangle1Side3) && (triangle1Side3 < triangle1Side1+triangle1Side2)){
+        AreaTriangle1 = abs(triangle1Ax *(triangle1By-triangle1Cy)+triangle1Bx*(triangle1Cy-triangle1Ay)+ triangle1Cx*(triangle1Ay-triangle1By))/2;
+        if (AreaTriangle1 >0 &&(abs(triangle1Side1-triangle1Side2) < triangle1Side3) && (triangle1Side3 < triangle1Side1+triangle1Side2)){
             printf("Triangle1 is created with (%f, %f), (%f, %f) and (%f, %f) points.",triangle1Ax,triangle1Ay, triangle1Bx,triangle1By, triangle1Cx,triangle1Cy);
             break;
         }
@@ -53,7 +54,8 @@ int main() {
         triangle2Side1 = sqrt(pow(triangle2Ax-triangle2Bx,2) + pow(triangle2Ay-triangle2By,2));
         triangle2Side2 = sqrt(pow(triangle2Ax-triangle2Cx,2) + pow(triangle2Ay-triangle2Cy,2));
         triangle2Side3 = sqrt(pow(triangle2Cx-triangle2Bx,2) + pow(triangle2Cy-triangle2By,2));
-        if ((abs(triangle2Side1-triangle2Side2) < triangle2Side3) && (triangle2Side3 < triangle2Side1+triangle2Side2)){
+        AreaTriangle2 = abs(triangle2Ax *(triangle2By-triangle2Cy)+triangle2Bx*(triangle2Cy-triangle2Ay)+ triangle2Cx*(triangle2Ay-triangle2By))/2;
+        if (AreaTriangle2 >0 && (abs(triangle2Side1-triangle2Side2) < triangle2Side3) && (triangle2Side3 < triangle2Side1+triangle2Side2)){
             printf("Triangle2 is created with (%f, %f), (%f, %f) and (%f, %f) points.",triangle2Ax,triangle2Ay, triangle2Bx,triangle2By, triangle2Cx,triangle2Cy);
             break;
         }
@@ -82,7 +84,9 @@ int main() {
         triangle3Side1 = sqrt(pow(triangle3Ax-triangle3Bx,2) + pow(triangle3Ay-triangle3By,2));
         triangle3Side2 = sqrt(pow(triangle3Ax-triangle3Cx,2) + pow(triangle3Ay-triangle3Cy,2));
         triangle3Side3 = sqrt(pow(triangle3Cx-triangle3Bx,2) + pow(triangle3Cy-triangle3By,2));
-        if ((abs(triangle3Side1-triangle3Side2) < triangle3Side3) && (triangle3Side3 < triangle3Side1+triangle3Side2)){
+        AreaTriangle3 = abs(triangle3Ax *(triangle3By-triangle3Cy)+triangle3Bx*(triangle3Cy-triangle3Ay)+ triangle3Cx*(triangle3Ay-triangle3By))/2;
+
+        if (AreaTriangle3>0 && (abs(triangle3Side1-triangle3Side2) < triangle3Side3) && (triangle3Side3 < triangle3Side1+triangle3Side2)){
             printf("Triangle3 is created with (%f, %f), (%f, %f) and (%f, %f) points.",triangle3Ax,triangle3Ay, triangle3Bx,triangle3By, triangle3Cx,triangle3Cy);
             break;
         }
@@ -98,12 +102,10 @@ int main() {
     printf("\nEnter Py:\n");
     scanf("%f",&Py);
     //If this point inside of this triangles then it means if we draw linet to this point to edges of triangles which include this point the sum of areas of triangles which occurs after we done that must be equal to area of the first triangle.
-    float AreaTriangle1,AreaTriangle2,AreaTriangle3,PAB1,PBC1,PAC1,PAB2,PBC2,PAC2,PAB3,PBC3,PAC3,sum1,sum2,sum3;
-    
-    AreaTriangle1 = abs(triangle1Ax *(triangle1By-triangle1Cy)+triangle1Bx*(triangle1Cy-triangle1Ay)+ triangle1Cx*(triangle1Ay-triangle1By))/2;
-    AreaTriangle2 = abs(triangle2Ax *(triangle2By-triangle2Cy)+triangle2Bx*(triangle2Cy-triangle2Ay)+ triangle2Cx*(triangle2Ay-triangle2By))/2;
-    AreaTriangle3 = abs(triangle3Ax *(triangle3By-triangle3Cy)+triangle3Bx*(triangle3Cy-triangle3Ay)+ triangle3Cx*(triangle3Ay-triangle3By))/2;
-    
+
+
+
+
     PAB1 = abs(Px*(triangle1Ay-triangle1By)+triangle1Ax*(triangle1By-Py)+triangle1Bx*(Py-triangle1Ay))/2;
     PBC1=abs(Px*(triangle1By-triangle1Cy)+triangle1Bx*(triangle1Cy-Py)+triangle1Cx*(Py-triangle1By))/2;
     PAC1 = abs(Px*(triangle1Ay-triangle1Cy)+triangle1Ax*(triangle1Cy-Py)+triangle1Cx*(Py-triangle1Ay))/2;
